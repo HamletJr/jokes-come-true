3796aab2efc872d1af5b47fe644abbd05b9509fc
HamletJr <jogosboy1902@gmail.com>
2024-09-18 11:20:13 +0700

    modify settings.py

--- jokes_come_true/settings.py
+++ jokes_come_true/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-455qtcy13v4qb0c@jyzf39nqh+g+#ldujj(_(7wsga3!7520qu'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["localhost", 
                  "127.0.0.1", 
